36bd28168b5da38208e4df8b6b7184c01995a439
LENOVO-PC\yonah <mets634@gmail.com>
2016-12-18 16:31:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/erel_yonah/findatrip/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.erel_yonah.findatrip;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String ACTION = "ACTION_UPDATE";
+    private static Integer counter = 0;
+    private BroadcastReceiver _refreshReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getExtras().getString("yonah");
+            ( (TextView) findViewById( R.id.showBroadcast ) ).setText(counter.toString() + ": " + text + intent.toString());
+            counter++;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerReceiver(_refreshReceiver, new IntentFilter(ACTION));
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(_refreshReceiver);
+    }
+}

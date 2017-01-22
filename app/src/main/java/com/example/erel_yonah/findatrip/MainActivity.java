62cf84753b8e307398eaffba84cd845ac5b644ea
DESKTOP-SO8TLE5\אראל <erelf7@gmail.com>
2017-01-22 16:51:49 +0200

    Start of the navigation drawer activity

--- app/src/main/java/com/example/erel_yonah/findatrip/MainActivity.java
+++ app/src/main/java/com/example/erel_yonah/findatrip/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.erel_yonah.findatrip;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Information has been updated successfully", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -80,13 +81,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_mainFragment) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_businesses) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_trips) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_exit) {
 
         } else if (id == R.id.nav_share) {
 

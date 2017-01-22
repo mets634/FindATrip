80e4a7b37cd5952d192745446ac753839618dd2c
DESKTOP-SO8TLE5\אראל <erelf7@gmail.com>
2017-01-22 15:28:01 +0200

    Start of the navigation drawer activity

---
+++ app/src/main/java/com/example/erel_yonah/findatrip/ReceiverActivity.java
@@ -0,0 +1,41 @@
+package com.example.erel_yonah.findatrip;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiverActivity extends AppCompatActivity {
+
+    private final static String ACTION = "ACTION_UPDATE";
+    private final static String EXTRA = "EXTRA";
+
+    private static Integer counter = 0;
+    private BroadcastReceiver _refreshReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            /*String text = intent.getExtras().getString(EXTRA);
+            ( (TextView) findViewById( R.id.showBroadcast ) ).setText(counter.toString() + ": " + text);
+            counter++;*/
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver);
+
+        registerReceiver(_refreshReceiver, new IntentFilter(ACTION));
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(_refreshReceiver);
+    }
+}
